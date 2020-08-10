30b46466f1e8fcb720ba8a9a606377571f1916b8
Joe Paul <jjp106@pitt.edu>
2020-08-10 17:10:58 -0400

    Added date extensions.

--- StockManagerServerApplication/App/Extensions/Foundation/Date/DateExtensions.swift
+++ StockManagerServerApplication/App/Extensions/Foundation/Date/DateExtensions.swift
@@ -1,8 +1,24 @@
 //
 //  DateExtensions.swift
-//  StockManagerServerApplication
+//  StockManagerWebServer
 //
 //  Created by Joe Paul on 8/10/20.
+//  Copyright © 2020 Zachary Grimaldi. All rights reserved.
 //
 
 import Foundation
+
+
+extension Date {
+    var sweeperOutputString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd_HH-mm-ss-a"
+        return dateFormatter.string(from: self)
+    }
+    
+    var viewOutputString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss:Z"
+        return dateFormatter.string(from: self)
+    }
+}
